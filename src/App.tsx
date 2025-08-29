9fa49704e6d30244b14831dbe5872fe38d3368e3
Yuri Zetoles <yurizetoles0123@gmail.com>
2025-08-28 21:33:07 -0400

    feat: adicionar dados e renderizar lista de nomes no componente App

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import "./App.css"
 import Card from "./components/Card"
+import Footer from "./components/Footer"
 import Header from "./components/Header"
+import { dados } from "./dados.ts"
 
 export default function App() {
   return (
     <div className="titulo">
       <Header />
+      <ul>
+        {dados.map(d => (
+          <li key={d.id}>{d.nome}</li>
+        ))}
+      </ul>
       <Card
         titulo="Produção de uva"
         texto="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Etiam eu egestas felis, et auctor neque. Vestibulum vel lorem velit. Sed ut fermentum odio."
         imagem="public/images/alface.png"
         link="https://github.com/YuriZetoles/PB_Compass/tree/main"
       />
+      <Footer />
     </div>
   )
 }
---
+++ src/dados.ts
@@ -0,0 +1,5 @@
+export const dados = [
+  {"id": 1, "nome": "Maria"},
+  {"id": 2, "nome": "João"},
+  {"id": 3, "nome": "José"}
+]
